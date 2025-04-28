b774e5d908ecb12e7ce45eea5ebdde6df529d6ae
kaushik <122958081+k1rbyd@users.noreply.github.com>
2025-04-28 17:07:31 +0530

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
-    port: process.env.PORT || 5173, // Use Render's port or fallback to 5173 for local dev
-    allowedHosts: ['ecoms-front.onrender.com', 'localhost'] // Allow your deployed host and localhost for dev
+    port: 10000, // Use Render's port or fallback to 5173 for local dev
+    allowedHosts: ['ecoms-front.onrender.com'] // Allow your deployed host and localhost for dev
   }
 })
