3e8fe764ac16c6fcb3a15be22d026d0f059a9ad4
kaushik <122958081+k1rbyd@users.noreply.github.com>
2025-04-28 16:26:07 +0530

    Update ProductCard.jsx

--- Client/src/components/ProductCard.jsx
+++ Client/src/components/ProductCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { assets } from "../assets/assets";
+import { assets } from "../assets/assets.js";
 import { useAppContext } from "../context/AppContext.jsx";
 
 const ProductCard = ({ product }) => {
