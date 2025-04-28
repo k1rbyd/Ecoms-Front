c4aeaceb8cd4efb762c6eb6c263f5fdd7f9efef5
kaushik <122958081+k1rbyd@users.noreply.github.com>
2025-04-28 15:54:42 +0530

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-  tailwindcss()
-  ]
+    tailwindcss()
+  ],
+  server: {
+    host: '0.0.0.0',    // Make Vite listen on all network interfaces
+    port: process.env.PORT || 5173  // Use Render's assigned port or fallback to 5173 for local dev
+  }
 })
