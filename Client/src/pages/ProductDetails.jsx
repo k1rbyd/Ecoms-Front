0f709a53b2658beb80aee44b7ed219bf0d9f06dc
kaushik <122958081+k1rbyd@users.noreply.github.com>
2025-04-28 16:26:35 +0530

    Update ProductDetails.jsx

--- Client/src/pages/ProductDetails.jsx
+++ Client/src/pages/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useAppContext } from "../context/AppContext.jsx";
 import { Link, useParams } from "react-router-dom";
-import { assets } from "../assets/assets";
+import { assets } from "../assets/assets.js";
 import ProductCard from "../components/ProductCard.jsx";
 
 const ProductDetails = () => {
