fa9078a4d54a9b685a204f0d8a10722dab5820eb
kaushik <122958081+k1rbyd@users.noreply.github.com>
2025-04-28 16:15:56 +0530

    Update AllProducts.jsx

--- Client/src/pages/AllProducts.jsx
+++ Client/src/pages/AllProducts.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useAppContext } from '../context/AppContext';
-import ProductCard from '../components/ProductCard';
+import { useAppContext } from '../context/AppContext.jsx';
+import ProductCard from '../components/ProductCard.jsx';
 
 const AllProducts = () => {
     const { products, searchQuery } = useAppContext();
