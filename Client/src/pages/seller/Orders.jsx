770f602ce2833fb8fb25bc3f53679c11fa566b70
kaushik <122958081+k1rbyd@users.noreply.github.com>
2025-04-28 16:17:13 +0530

    Update Orders.jsx

--- Client/src/pages/seller/Orders.jsx
+++ Client/src/pages/seller/Orders.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useAppContext } from '../../context/AppContext';
+import { useAppContext } from '../../context/AppContext.jsx';
 import { assets, dummyOrders } from '../../assets/assets';
 import axios from 'axios';
 import toast from 'react-hot-toast';
